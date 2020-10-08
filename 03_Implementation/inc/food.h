54d73429be26289476826f49566426dbf810e5da
stepin104261 <69055585+stepin104261@users.noreply.github.com>
2020-10-09 01:55:07 +0530

    Update food.h

--- 03_Implementation/inc/food.h
+++ 03_Implementation/inc/food.h
@@ -9,34 +9,34 @@
 //#include<time.h>
 
 /// Update & Insert Function
-void insertfirst(int data, char foodname[25], int quantity, float price);
-void insertmid(int pos, int data, char foodname[25], int quantity, float price);
-void insertend(int data, char foodname[25], int quantity, float price);
-void updatefood(int udata, int uquantity);
+void insertfirst();
+void insertmid();
+void insertend();
+void updatefood();
 
      /// Display Function
 void foodlist();
-void order_view(int order, int quantity, int or_no);
+void order_view();
 void main_menu();
 
     ///Delete & Count Function
 
-int deletefood(int serial);
+int deletefood();
 int countitem();
 
 ///extra design Function
 
 void cls();
-void echo(char print[]);
-void br(int line);
-void pre(int tab);
-void span(int space);
+void echo();
+void br();
+void pre();
+void span();
 //void ccolor(int clr);
 void pwelcome();
 void loadingbar();
-void middle1(void);
-void middtab1(void);
-void backuploader(void);
+void middle1();
+void middtab1();
+void backuploader();
 
 typedef struct Node node ;
 
